982997b425e55e0023e539dbc912a955c42bb5cf
panjianghui <546540274@qq.com>
2018-03-21 20:05:17 +0800

    20180321 - 网络请求工具类

--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
 <h3>**spring+srping mvc+mybatis整合** <h3/>
+**计划安排：**    
+    <li1>
+    1、实现mybatis链表查询功能<br>
+    2、集成dubbo分布式框架<li1/>
+    
--- src/main/test/TestTimeList.java
+++ src/main/test/TestTimeList.java
@@ -1,7 +1,14 @@
+import com.jh.service.UserService;
 import junit.framework.TestCase;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestTimeList extends TestCase {
-    public void testGetTimeList(){
 
+    public void testGetTimeList(){
+        System.setProperty("java.net.preferIPv4Stack","true");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"applicationContext.xml"});
+        context.start();
+        UserService user = (UserService)context.getBean("userService");
+        user.getTimeList();
     }
 }
